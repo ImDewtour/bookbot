a4dea0f4dbf777528cf3ea8d38d4b647eebdf733
ImDewtour <truedewtour@gmail.com>
2025-06-25 16:00:31 -0400

    Character sorting - bookbot printout

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from stats import get_num_words, character_count
+from stats import get_num_words, character_count, sorted_chars, bookbot
 
 def main():
     book_path = "books/frankenstein.txt"
     text = (get_book_text(book_path))
     word_count = get_num_words(text)
     char_dict = character_count(text)
-    print(f"{word_count} words found in the document")
-    print(char_dict)
+    sort_dict = sorted_chars(char_dict)
+#    print(f"{word_count} words found in the document")
+#    print(char_dict.sort)
+    bookbot(book_path, word_count, sort_dict)
+
 
 def get_book_text(path):
     with open(path) as fp:
         return fp.read()
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,6 @@
 def get_num_words(text):
-    wc = len(text.split())
-    return wc
+    word_count = len(text.split())
+    return word_count
 
 def character_count(text):
     chars = {}
@@ -11,3 +11,23 @@ def character_count(text):
         else:
             chars[char] += 1
     return chars
+
+def sorted_chars(char_dict):
+    sorted_list = sorted(char_dict.items(), key=lambda item: item[1], reverse=True)
+    sorted_dict = {}
+    for char, count in sorted_list:
+        if char.isalpha():
+            sorted_dict[char] = count
+    return sorted_dict
+
+
+
+def bookbot(book_path, word_count, sort_dict):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for char in sort_dict:
+        print(f"{char[0]}: {sort_dict[char]}")
+    print("============= END ===============")
